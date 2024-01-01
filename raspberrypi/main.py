c1b12d0151a00bf214c44c9951ee0f0d2db143bb
sirishant <99063193+sirishant@users.noreply.github.com>
2024-01-01 19:16:31 +0530

    Update main.py

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -1,7 +1,14 @@
 import espCamAccess
+import face_recgn2
 
 def main():
-        espCamAccess.accessCam()
+        addr, pw = espCamAccess.getCreds()
+        print(f"Address: {addr}, PW: {pw}")
+        espCamAccess.accessCam(addr, pw)
+        namesDict = face_recgn2.checkImg()
+        recognised = max(zip(namesDict.values(), namesDict.keys()))[1]
+        if recognised != 'Unknown':
+            espCamAccess.openLock(addr, pw)
         return
 
 if __name__ == '__main__':
