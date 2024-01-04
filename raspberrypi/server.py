c33c454a28c1e93b047f891609b10964bd83ea84
sirishant <99063193+sirishant@users.noreply.github.com>
2024-01-04 19:29:57 +0530

    Update server.py
    getLine() to receive button push message from ESPCam, and handle "Address already in use"

--- raspberrypi/server.py
+++ raspberrypi/server.py
@@ -5,11 +5,35 @@ import socket
 import struct
 
 # ENTER 192.168.0.18 FOR RASPBERRY PI
-HOST = "192.168.0.16"  # Standard loopback interface address (localhost)
+HOST = "192.168.0.20"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+def getLine():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST, PORT))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)       #Get Password from ESP
+                if not data:
+                    break
+                # conn.sendall(bytes("Doorbell rung!", 'utf-8'))    # Send response to ESP32Cam
+                print(f"Received", data.decode())               # Decode the password received
+                # conn.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 0))
+                # conn.close()
+                s.shutdown(0)
+                s.close()            # Close connection
+              
+                return data.decode()    # Returns the Message
+
 
 def getPassword():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.listen()
@@ -21,9 +45,10 @@ def getPassword():
                 if not data:
                     break
                 conn.sendall(bytes("PW Received!", 'utf-8'))    # Send response to ESP32Cam
-                print(f"Received", data.decode())               # Decode the password received
+                print(f"Received PW", data.decode())               # Decode the password received
                 # conn.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 0))
                 # conn.close()
+                s.shutdown(0)
                 s.close()            # Close connection
               
                 return data.decode(), addr    # Returns the Password and [IP Address, Port No]
