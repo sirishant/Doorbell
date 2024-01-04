e6bbd180ebff1153dfaeb62192422ef19368fb67
sirishant <99063193+sirishant@users.noreply.github.com>
2024-01-04 19:26:15 +0530

    Update main.py
    Make server to be always on, and handle loophole of "no face detected"

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -1,16 +1,40 @@
 import espCamAccess
 import face_recgn2
+import time
+
+def getConn():
+                time.sleep(1)
+                addr, pw = espCamAccess.getCreds()
+                if pw == 'DINGDONG':
+                        return
+                print(f"Address: {addr}, PW: {pw}")
+                if (espCamAccess.readLine() == 'DINGDONG'):
+                        espCamAccess.accessCam(addr, pw)
+                        namesDict = face_recgn2.checkImg()
+                        flag=0                          # Try improving logic for seeing if
+                        for value in namesDict.values():  # all values are zero
+                                if value != 0:
+                                        flag = 1
+                        if flag == 1:
+                                recognised = max(zip(namesDict.values(), namesDict.keys()))[1]
+                        else:
+                                recognised = 'Unknown'
+                        print(f"Recognised as {recognised}!")
+                        if recognised != 'Unknown':
+                                espCamAccess.openLock(addr, pw)
+                                time.sleep(.25)
+                                print("Unlocked!")
+                        print("Logging out..")
+                        espCamAccess.logout(addr)
+                        print("Logged out!")
+                        return
 
 def main():
-        addr, pw = espCamAccess.getCreds()
-        print(f"Address: {addr}, PW: {pw}")
-        espCamAccess.accessCam(addr, pw)
-        namesDict = face_recgn2.checkImg()
-        recognised = max(zip(namesDict.values(), namesDict.keys()))[1]
-        if recognised != 'Unknown':
-            espCamAccess.openLock(addr, pw)
-        return
+        while(1):
+                time.sleep(2)
+                print("Fetching new connection..")
+                getConn()
 
 if __name__ == '__main__':
-        while True:
-                main()
+        time.sleep(3)
+        main()
